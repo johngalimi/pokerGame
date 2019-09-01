93f94f7b587dddbd55738addbf06de89adbb0204
johngalimi <galimijohn@gmail.com>
2019-09-01 13:48:07 -0400

    added dealer method and display method

--- cards.py
+++ cards.py
@@ -7,6 +7,7 @@ class Card:
 		self.suit = suit
 		self.rank = rank
 
+	# method to view card attributes
 	def view_card(self):
 		print(f'{self.rank} of {self.suit}')
 
@@ -17,24 +18,44 @@ class Deck:
 	def __init__(self):
 		self.cardDeck = []
 		self.generate_deck()
+		self.shuffle_deck()
 		
-
+	# method to generate deck
 	def generate_deck(self):
-
 		for i in ['D', 'H', 'C', 'S']:
 			for j in range(1, 14):
 				self.cardDeck.append(Card(i, j))
 
+	# method to shuffle deck
+	def shuffle_deck(self):
+		random.shuffle(self.cardDeck)
 
+	# method to get num cards remaining - check when dealing
 	def get_cards_remaining(self):
 		return len(self.cardDeck)
 
-	def shuffle(self):
-		pass
+	# method to view entire deck
+	def view_deck(self):
+		for deck_card in self.cardDeck:
+			deck_card.view_card()
+
+	# method to deal single card
+	def deal_card(self):
+		return self.cardDeck.pop()
 
 
 if __name__ == '__main__':
+	
 	my_deck = Deck()
 
-	for card in my_deck.cardDeck:
-		card.view_card()
+	my_deck.view_deck()
+
+	print('-----------dealing')
+
+	dealt_1 = my_deck.deal_card()
+	dealt_2 = my_deck.deal_card()
+
+	dealt_1.view_card()
+	dealt_2.view_card()
+
+	print(my_deck.get_cards_remaining())
