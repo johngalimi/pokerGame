45cf2b7dad42d08a5940152bd9e16341fc27dd4b
johngalimi <galimijohn@gmail.com>
2019-09-05 13:19:08 -0400

    working on deck implementation

---
+++ java/Deck.java
@@ -0,0 +1,12 @@
+public class Deck {
+	int[] cardDeck;
+
+	public Deck(int[] cardDeck) {
+		this.cardDeck = int[52];
+	}
+
+	public static void main(String[] args) {
+		Deck john = new Deck();
+	}
+
+}
