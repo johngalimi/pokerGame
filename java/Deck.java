fce0368d5ca46e72152600cc7ce340ef57360869
johngalimi <galimijohn@gmail.com>
2019-09-06 17:58:22 -0400

    implemented deck

--- java/Deck.java
+++ java/Deck.java
@@ -1,12 +1,30 @@
 public class Deck {
-	int[] cardDeck;
+	Card[] cardDeck = new Card[52];
 
-	public Deck(int[] cardDeck) {
-		this.cardDeck = int[52];
+	public Deck() {
+		this.cardDeck = cardDeck;
+	}
+
+	public void generateDeck() {
+
+		String[] suits = new String[] { "D", "H", "C", "S" };
+		int count = 0;
+
+		for (String suit : suits) {
+			for (int rank = 2; rank < 15; rank++) {
+				cardDeck[count] = new Card(suit, rank);
+				count++;
+			}
+		}
+		
+		for (Card card : cardDeck) {
+			System.out.println(card.viewCard());
+		}
 	}
 
 	public static void main(String[] args) {
 		Deck john = new Deck();
+		john.generateDeck();
 	}
 
 }
